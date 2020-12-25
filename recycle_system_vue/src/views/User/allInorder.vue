f1c863a4c29799253100ad8abfabc523a1634a79
vicdann <1318795439@qq.com>
2020-12-25 23:46:51 +0800

    update allinorder
    vue添加根据快递员查询功能

--- recycle_system_vue/src/views/User/allInorder.vue
+++ recycle_system_vue/src/views/User/allInorder.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-
+    根据快递员姓名查询订单:<el-input v-model="cellectorname" placeholder="请输入快递员姓名" style="width: 200px"></el-input>
+    <el-button  type="primary" round slot="reference" @click="findbycellectorname(cellectorname)">查询</el-button>
     <el-table :data="tableData">
       <el-table-column prop="recycleOrderId" label="订单号" width="140">
       </el-table-column>
@@ -41,76 +42,88 @@
 </template>
 
 <script>
-  export default {
-    methods:{
-      showDetail(recycleOrderId){
-        const _this = this
-        axios.get('http://localhost:8181/OrdersDetail/'+recycleOrderId+'').then(function (resp) {
-          _this.recycleOrdersDetailVoList=resp.data
-          for(var j=0,len=_this.recycleOrdersDetailVoList.length;j<len;j++){
-            _this.recycleOrdersDetailVoList[j].sum = _this.recycleOrdersDetailVoList[j].quantity*_this.recycleOrdersDetailVoList[j].itemPrice
-          }
-        })
-      },
-      page(currentPage){
-        const _this = this
-        axios.get('http://localhost:8181/userAllorders/'+_this.$store.getters.getUserId+'/'+currentPage+'/2').then(function(resp){
-          _this.tableData = resp.data.list
-          _this.pageSize = resp.data.pageSize
-          _this.total = resp.data.total
-        })
-      }
+export default {
+  methods: {
+    showDetail (recycleOrderId) {
+      const _this = this
+      // eslint-disable-next-line no-undef
+      axios.get('http://localhost:8181/OrdersDetail/' + recycleOrderId + '').then(function (resp) {
+        _this.recycleOrdersDetailVoList = resp.data
+        for (var j = 0, len = _this.recycleOrdersDetailVoList.length; j < len; j++) {
+          _this.recycleOrdersDetailVoList[j].sum = _this.recycleOrdersDetailVoList[j].quantity * _this.recycleOrdersDetailVoList[j].itemPrice
+        }
+      })
     },
-    created () {
-      const _this=this;
-      axios.get('http://localhost:8181/userAllorders/'+_this.$store.getters.getUserId+'/1/2').then(function (resp) {
-        _this.tableData=resp.data.list
+    findbycellectorname (cellectorname) {
+      const _this = this
+      // eslint-disable-next-line no-undef
+      axios.get('http://localhost:8181/userFindordersBycellectorname/' + _this.$store.getters.getUserId + '/' + cellectorname + '/1' + '/2').then(function (resp) {
+        _this.tableData = resp.data.list
         _this.pageSize = resp.data.pageSize
         _this.total = resp.data.total
       })
     },
-    data(){
-      return{
-        pageSize:1,
-        total:1,
-        recycleOrdersDetailVoList:[{
-          itemName: '纸板',
-          quantity: 10,
-          itemPrice: 1,
-          sum:'',
-        }, {
-          itemName: '易拉罐',
-          quantity: 20,
-          itemPrice: 0.1,
-          sum:'',
-        }, {
-          itemName: '啤酒瓶',
-          quantity: 5,
-          itemPrice: 1,
-          sum:'',
-        }, {
-          itemName: '旧衣服',
-          quantity: 20,
-          itemPrice: 0.5,
-          sum:'',
-        }],
-        tableData: [{
-          recycleOrderId: 1,
-          scheduledTime: '12月15日 下午17：00',
-          finishedTime: '12月15日 下午17：10',
-          collectorName: '陈南',
-          phone: 13615787610,
-        },
-          {
-          recycleOrderId: 2,
-          scheduledTime: '12月15日 下午17：00',
-          finishedTime:'12月15日 下午17：10',
-          collectorName: '陈南',
-          phone:13615787610,
-        }]
-      }
+    page (currentPage) {
+      const _this = this
+      // eslint-disable-next-line no-undef
+      axios.get('http://localhost:8181/userAllorders/' + _this.$store.getters.getUserId + '/' + currentPage + '/2').then(function (resp) {
+        _this.tableData = resp.data.list
+        _this.pageSize = resp.data.pageSize
+        _this.total = resp.data.total
+      })
+    }
+  },
+  created () {
+    const _this = this
+    // eslint-disable-next-line no-undef
+    axios.get('http://localhost:8181/userAllorders/' + _this.$store.getters.getUserId + '/1/2').then(function (resp) {
+      _this.tableData = resp.data.list
+      _this.pageSize = resp.data.pageSize
+      _this.total = resp.data.total
+    })
+  },
+  data () {
+    return {
+      pageSize: 1,
+      total: 1,
+      recycleOrdersDetailVoList: [{
+        itemName: '纸板',
+        quantity: 10,
+        itemPrice: 1,
+        sum: ''
+      }, {
+        itemName: '易拉罐',
+        quantity: 20,
+        itemPrice: 0.1,
+        sum: ''
+      }, {
+        itemName: '啤酒瓶',
+        quantity: 5,
+        itemPrice: 1,
+        sum: ''
+      }, {
+        itemName: '旧衣服',
+        quantity: 20,
+        itemPrice: 0.5,
+        sum: ''
+      }],
+      tableData: [{
+        recycleOrderId: 1,
+        scheduledTime: '12月15日 下午17：00',
+        finishedTime: '12月15日 下午17：10',
+        collectorName: '陈南',
+        phone: 13615787610
+      },
+      {
+        recycleOrderId: 2,
+        scheduledTime: '12月15日 下午17：00',
+        finishedTime: '12月15日 下午17：10',
+        collectorName: '陈南',
+        phone: 13615787610
+      }]
     }
   }
+}
 </script>
 
 <style scoped>
